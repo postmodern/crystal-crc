9ce566c5d8dab863a313a869db9e18d9403dd6c1
Postmodern <postmodern.mod3@gmail.com>
2021-01-27 10:20:27 -0800

    Digest::Base became Digest in Crystal 0.36.0.

--- src/digest/crc.cr
+++ src/digest/crc.cr
@@ -1,78 +1,76 @@
-require "digest/base"
+require "digest/digest"
 
-module Digest
-  abstract class CRC(UINT) < ::Digest::Base
+abstract class Digest::CRC(UINT) < ::Digest
 
-    #
-    # The default initial CRC value.
-    #
-    def self.init_crc : UINT
-      UINT.new(0)
-    end
-
-    #
-    # The default CRC XOR mask.
-    #
-    def self.xor_mask : UINT
-      UINT.new(0)
-    end
-
-    #
-    # Initializes the CRC instance.
-    #
-    def initialize(@crc : UINT = self.class.init_crc)
-    end
-
-    #
-    # Calculates the CRC checksum.
-    #
-    def self.checksum(data) : UINT
-      crc = self.new
-      crc << data
-
-      crc.final
-      crc.checksum
-    end
-
-    #
-    # @see #update
-    #
-    @[AlwaysInline]
-    def <<(data)
-      update(data)
-      self
-    end
-
-    #
-    # The resulting CRC checksum.
-    #
-    def checksum : UINT
-      @crc ^ self.class.xor_mask
-    end
-
-    #
-    # Updates the CRC checksum with the given data.
-    #
-    abstract def update_impl(data : Bytes) : Nil
-
-    #
-    # Packs the final CRC result.
-    #
-    abstract def final_impl(dst : Bytes) : Nil
-
-    #
-    # Resets the CRC checksum.
-    #
-    def reset_impl : Nil
-      @crc = self.class.init_crc
-    end
-
-    #
-    # The size of the resulting CRC checksum.
-    #
-    def digest_size : Int32
-      sizeof(UINT)
-    end
-
+  #
+  # The default initial CRC value.
+  #
+  def self.init_crc : UINT
+    UINT.new(0)
   end
+
+  #
+  # The default CRC XOR mask.
+  #
+  def self.xor_mask : UINT
+    UINT.new(0)
+  end
+
+  #
+  # Initializes the CRC instance.
+  #
+  def initialize(@crc : UINT = self.class.init_crc)
+  end
+
+  #
+  # Calculates the CRC checksum.
+  #
+  def self.checksum(data) : UINT
+    crc = self.new
+    crc << data
+
+    crc.final
+    crc.checksum
+  end
+
+  #
+  # @see #update
+  #
+  @[AlwaysInline]
+  def <<(data)
+    update(data)
+    self
+  end
+
+  #
+  # The resulting CRC checksum.
+  #
+  def checksum : UINT
+    @crc ^ self.class.xor_mask
+  end
+
+  #
+  # Updates the CRC checksum with the given data.
+  #
+  abstract def update_impl(data : Bytes) : Nil
+
+  #
+  # Packs the final CRC result.
+  #
+  abstract def final_impl(dst : Bytes) : Nil
+
+  #
+  # Resets the CRC checksum.
+  #
+  def reset_impl : Nil
+    @crc = self.class.init_crc
+  end
+
+  #
+  # The size of the resulting CRC checksum.
+  #
+  def digest_size : Int32
+    sizeof(UINT)
+  end
+
 end
